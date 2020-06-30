4e4e318e1606590ed33d26055288586a40968be1
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-06-29 22:04:46 -0300

    configurar ambiente java, gradle e maven

---
+++ aulaJava/example-maven/src/test/java/br/com/globallabs/examplemaven/ExampleMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.globallabs.examplemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
