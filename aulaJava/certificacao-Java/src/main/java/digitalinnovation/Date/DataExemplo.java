bfd5cabdd9071e9dc99f04e77141b567933f5407
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-07-04 17:25:08 -0300

    clonagem dos repositorios de exemplo java

---
+++ aulaJava/certificacao-Java/src/main/java/digitalinnovation/Date/DataExemplo.java
@@ -0,0 +1,10 @@
+package digitalinnovation.Date;
+import java.util.Date;
+
+public class DataExemplo {
+        public static void main(String[] args){
+            Date novaData = new Date();
+            System.out.println("A data é: "+ novaData);
+        }
+    }
+
