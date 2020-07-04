167b0c9b12a4ba3d05e2a8eb3ea3ca0f71aa5812
wagnerrafa <wagner_rafa@hotmail.com.br>
2020-07-04 17:57:02 -0300

    exemplos de uso do Date

--- aulaJava/certificacao-Java/src/main/java/digitalinnovation/Date/DataExemplo.java
+++ aulaJava/certificacao-Java/src/main/java/digitalinnovation/Date/DataExemplo.java
@@ -1,10 +1,18 @@
 package digitalinnovation.Date;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class DataExemplo {
         public static void main(String[] args){
             Date novaData = new Date();
-            System.out.println("A data é: "+ novaData);
+            System.out.println("A data no formato antigo é é: " + novaData);
+
+            LocalDateTime agora = LocalDateTime.now();
+            System.out.println("A data no formato atual é: " + agora);
+
+            LocalDateTime futuro = agora.plusHours(1).plusDays(2).plusSeconds(12);
+            System.out.println("A soma das datas é: " + futuro);
         }
+
     }
 
